a2db40546bef1d267989f2b0fefe33d644fdf9d2
Asmaa Bennassef <bennassefasmaa@gmail.com>
2021-06-28 16:59:44 +0200

    TEST Asmaa

--- src/main/java/fr/doranco/ecommerce/entity/LignePanier.java
+++ src/main/java/fr/doranco/ecommerce/entity/LignePanier.java
@@ -5,12 +5,6 @@ import java.io.Serializable;
 public class LignePanier implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
-	
-<<<<<<< HEAD
-	////test Fatou
-	//Comlmentaire 2
-=======
-	////tEST COMMENTAIRE
->>>>>>> branch 'Fatou' of https://github.com/dfatoumata/eCommerce.git
+// test 123
 
 }
